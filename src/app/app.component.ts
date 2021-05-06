37e31141adde19a51a534996fadb0a96360ea2c6
ansi123 <mohitsinghalcool1998@gmail.com>
2021-05-06 15:55:27 +0530

    lazy loading using vcr and cfr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+// import { Component, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
+// import { AdminListComponent } from './admin-list/admin-list.component';
+// import { UserListComponent } from './user-list/user-list.component';
+ import { Component,ViewContainerRef,ComponentFactoryResolver} from '@angular/core';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+
+
+
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'decorators';
+  constructor(
+    // private vcR: ViewContainerRef, //Responsible for making a container where we can just place the lazyloaded component
+    // private cfR: ComponentFactoryResolver //will make the component from the code
+
+    private vcR: ViewContainerRef,
+    private cfR: ComponentFactoryResolver
+  ) {
+  }
+  // async loadAdmin(){
+  //   this.vcR.clear();
+  //   const {} = await import('./admin-list/admin-list.component')
+  //   this.vcR.createComponent(
+  //     this.cfR.resolveComponentFactory(AdminListComponent)
+  //   )
+  // }
+  // async loadUser(){
+  //   this.vcR.clear();
+  //   const {} = await import('./user-list/user-list.component')
+  //   this.vcR.createComponent(
+  //     this.cfR.resolveComponentFactory(UserListComponent)
+  //   )
+  // }
+
+  async loadAdmin(){
+    this.vcR.clear();
+    const {} =await import('./admin/admin.component')
+    this.vcR.createComponent(
+
+      this.cfR.resolveComponentFactory(AdminComponent)
+    )
+  }
+
+  async loadUser(){
+    this.vcR.clear();
+    const {} = await import('./user/user.component');
+    this.vcR.createComponent(
+      this.cfR.resolveComponentFactory(UserComponent)
+    )
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+// import { AdminListComponent } from './admin-list/admin-list.component';
+// import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AdminComponent,
+    // AdminListComponent,
+    // UserListComponent
   ],
   imports: [
     BrowserModule,
